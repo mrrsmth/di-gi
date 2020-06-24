66e3bdec0c08e0576cd08b375419cc9c4308abf0
alxs1dd93 <alxs1dd93@gmail.com>
2020-06-24 19:31:16 +0300

    YA COMMIT!!111111

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false//!выкл стрелочки при 640px
+                }
+            }
+        ]
+    });
+});
