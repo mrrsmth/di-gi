ace5504ae387b965e788211317b7d84a7eb6fdcb
alxs1dd93 <alxs1dd93@gmail.com>
2020-06-25 14:31:43 +0300

    Ситилизовал кусок адаптива слайдера по уроку.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(function () {
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next-arrow.svg" alt="prev"></button>', //!добавляем стрелки
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev-arrow.svg" alt="next"></button>', //!добавляем стрелки
         responsive: [{
                 breakpoint: 1024,
                 settings: {
@@ -12,7 +14,7 @@ $(function () {
             {
                 breakpoint: 640,
                 settings: {
-                    arrows: false//!выкл стрелочки при 640px
+                    arrows: false //!выкл стрелочки при 640px
                 }
             }
         ]
